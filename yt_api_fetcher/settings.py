e9fcd577f20032251f383b8d5c576d549d909958
Manthan Ghasadiya <68530736+manthanghasadiya@users.noreply.github.com>
2024-12-26 18:53:02 +0530

    sec updates

--- yt_api_fetcher/settings.py
+++ yt_api_fetcher/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)jee%q@^mb=tj5gzzna7%o2&nh%_ql4#0zmtwbxd@)3zs-7v52'
+SECRET_KEY = 'SHHH... ITS SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
